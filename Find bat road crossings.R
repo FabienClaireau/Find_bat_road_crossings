4496f447cbf1f826d626f9def9782e37467f8fb1
Fabien CLAIREAU <fclaireau@mnhn.fr>
2018-11-11 15:15:47 +0100

    Update Find bat road crossings.R

--- Find bat road crossings.R
+++ Find bat road crossings.R
@@ -27,20 +27,20 @@ ListPaires=read.csv2("ListPairs_Dec_Man.csv")
 TravFinal=vector()
 
 for (h in 1:nrow(ListPaires)){
-  TrajTot1=subset(TrajTot, Participation %in% ListPaires$participation_1[h])
+  TrajTot1=subset(TrajTot, participation %in% ListPaires$participation_1[h])
   TrajTot1=cbind(TrajTot1,Cote=1, Point=ListPaires$point_1[h])
-  TrajTot2=subset(TrajTot, Participation %in% ListPaires$participation_2[h])
+  TrajTot2=subset(TrajTot, participation %in% ListPaires$participation_2[h])
   TrajTot2=cbind(TrajTot2,Cote=2, Point=ListPaires$point_2[h])
   Trav=rbind(TrajTot1,TrajTot2)
   Trav$DecDeb=as.numeric(as.character(Trav$DecDeb))
   Trav$DecFin=as.numeric(as.character(Trav$DecFin))
-  Trav$Prob=as.numeric(as.character(Trav$Prob))
+  Trav$Prob=as.numeric(as.character(Trav$probabilite))
   Date=vector(length=nrow(Trav))
   Heure=vector(length=nrow(Trav))
   Minute=vector(length=nrow(Trav))
   Seconde=vector(length=nrow(Trav))
   MiliSec=vector(length=nrow(Trav))
-  Fich=as.character(Trav$File)
+  Fich=as.character(Trav$File0)
   for (i in 1:length(Fich)){
     Date[i]=substr(Fich[i],nchar(Fich[i])-18,nchar(Fich[i])-11)
     Heure[i]=substr(Fich[i],nchar(Fich[i])-9,nchar(Fich[i])-8)
@@ -70,8 +70,8 @@ for (h in 1:nrow(ListPaires)){
     library(Hmisc)
     if((((nrow(Ent1)>0)&(nrow(Sort2)>0))))
     {
-      TC12=find.matches(cbind(Ent1$Datenum,Ent1$Species,Ent1$Temps)
-                        ,cbind(Sort2$Datenum,Sort2$Species,Sort2$Temps)
+      TC12=find.matches(cbind(Ent1$Datenum,Ent1$espece,Ent1$Temps)
+                        ,cbind(Sort2$Datenum,Sort2$espece,Sort2$Temps)
                         ,tol=c(0,0,(ListPaires$inter_distance[h]/5+(ListPaires$inter_distance[h]/5*0.5)+5))
                         ,maxmatch=1)
       DecMesure12=vector()
@@ -85,8 +85,8 @@ for (h in 1:nrow(ListPaires)){
     }
     if((((nrow(Ent2)>0)&(nrow(Sort1)>0))))
     {
-      TC21=find.matches(cbind(Ent2$Datenum,Ent2$Species,Ent2$Temps)
-                        ,cbind(Sort1$Datenum,Sort1$Species,Sort1$Temps)
+      TC21=find.matches(cbind(Ent2$Datenum,Ent2$espece,Ent2$Temps)
+                        ,cbind(Sort1$Datenum,Sort1$espece,Sort1$Temps)
                         ,tol=c(0,0,(ListPaires$inter_distance[h]/5+(ListPaires$inter_distance[h]/5*0.5)+5))
                         ,maxmatch=1)
       DecMesure21=vector()
@@ -117,4 +117,5 @@ for (h in 1:nrow(ListPaires)){
   }
 }
 
+#File containing bat crossing
 write.csv2(TravFinal,"TravFinal.csv")
